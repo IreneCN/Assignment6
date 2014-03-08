e20bf7113ede165406ba7eac07b82cf9eefb0836
Irene <icn2104@columbia.edu>
2014-03-07 20:22:05 -0500

    Made changes to my javascript file in order to load google viz

--- scripts.js
+++ scripts.js
@@ -6,79 +6,51 @@
  1. Retrieve data from FRED
  2. Create a Google Fusion table
  3. Change the first $-get request i.e. put in the link to Google url
- 4. I created a header array for the column I want to create
- 5. 
+ 4. I created a Google data table
+ 5. I've added rows and columns
+ 6. I created an object, myChartOptions, to assign a title to the Line Chart I';; draw
+ 7. 
  */
 
-function mydataUp(unEMPDATA){            //uEMPDATA is the local name for the JSON file
-	
-	console.log(unEMPDATA);        //I'm console-logging unEMPDATA to be sure the function dataUp works on my javascript
-	
-	
-	var ArrayHeader = unEMPDATA.columns; //I'm creaating the header for my arrays
-	
-	var gDataTable = new google.visualization.DataTable(); // I've included a new Google table to access my visualization data
-	// The 2nd parameter is the name of the column
-	gDataTable.addColumn('string', ArrayHeader[0]);  //I'm adding columns and the 1st parameter is the string datatype
-	gDataTable.addColumn('number', ArrayHeader[1]);  //I'm adding a second parameter of the number datatype
-	
-	gDataTable.addRows(unEMPDATA.rows);
-	
-	
-	var myChartOptions = {                           //Created options object to actually customize the look of my chart
-          title: "Unemployment Insurance Weekly Claims Report Since 1980"       //This is a configuration option
-        };
-	//tells Google Visualization to create a line chart and give it to the
-	var gThisChart = new google.visualization.LineChart(document.getElementById('myNewChartDiv')); //I've created type of chart e.g. BarChart to LineChart etc
-	gThisChart.draw(gDataTable, myChartOptions);
 
-
-
-}
-
-/*function mydataUp2(unEMPDATA){            //uEMPDATA is the local name for the JSON file and I'm including a second visualization
+function mydataUp(unEMPDATA){       //unEMPDATA is the local name for the JSON file
 	
 	console.log(unEMPDATA);        //I'm outputting the JSON file to be sure the function dataUp works on my javascript
 	
-	var gDataTable = new google.visualization.DataTable();
+	var gDataTable = new google.visualization.DataTable(); // I'm creating a table for my data from Google
 	
 	//When I add columns, the 1st parameter is the data type in that column
 	// The 2nd parameter is the name of the column
 	gDataTable.addColumn('string', unEMPDATA.columns[0]);
 	gDataTable.addColumn('number', unEMPDATA.columns[1]);
 	
-	gDataTable.addRows(unEMPDATA.rows);
+	gDataTable.addRows(unEMPDATA.rows); //I'm including rows into my Google Data Table
 	
 	
-	var myChartChoices = {                           //create options object to actually customize the look of our chart
-          title: ""       //This is a configuration option
+	var myChartOptions = {                           //create options object to actually customize the look of our chart
+          title: "Unemployment Since 1980"       //This is a configuration option
         };
 	//tells Google Visualization to create a line chart and give it to the
-	var ThisChart = new google.visualization.LineChart(document.getElementById('myNewChartDiv2')); //I've created type of chart e.g. BarChart to LineChart etc
-	ThisChart.draw(gDataTable,ChartChoices);
+	var gThisChart = new google.visualization.LineChart(document.getElementById('myNewChartDiv')); //I've created type of chart e.g. BarChart to LineChart etc
+	gThisChart.draw(gDataTable,myChartOptions); //This is the operation that draws my Line Chart
 
 
 
 }
-*/
 
-function myGoogleUp() {
+
+function myGoogleUp() {       //This is my function to retrieve my Google data
 
 	console.log("I've put up Google!");
 	
-	// Instead of loading data from a static json file
-	//I'm going to load it from Google Fusion Table	
 	
-	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+10wtbAdS9Jlnpm2Xj7ibzvFM1jTBE3JTRQgbCkciA+WHERE+DATE>'1979-12-31'&key=AIzaSyAm9yWCV7JPCTHCJut8whOjARd7pwROFDQ", mydataUp, "json"); //I'm retrieving my the json 
-	
-	//We're including a second visualization and so we duplicated the function that loads data and created a new div in my html file
-	//$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1L1rmYMaUAgK7tXaB9vt28njZA3ZAj4k8zHizBP9N+WHERE+DATE>'1971-12-01'&key=AIzaSyCgfAUYUUuX8vurL0WtKGYRGho11LEnsPI", dataUp2, "json");
-	
+	//I'm loading data from my Google Fusion Table and retrieving my the json 
+	$.get("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT+*+FROM+1L1rmYMaUAgK7tXaB9vt28njZA3ZAj4k8zHizBP9N+WHERE+DATE>'1971-12-01'&key=AIzaSyCgfAUYUUuX8vurL0WtKGYRGho11LEnsPI", mydataUp, "json"); 
 	
 	}
 
 
-function mydocUp() {
+function mydocUp() {             //This function is for uploading my document
 
 	console.log("My page is up!");
 
